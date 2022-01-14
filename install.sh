3bf240f2f31462a9c2a5864b74bd4ed20c45752d
Chris Keating <pdex@google.com>
2022-01-14 22:22:07 +0000

    WIP: changes to support automating solution

--- install.sh
+++ install.sh
@@ -24,6 +24,11 @@ Usage:
 Options:
   --project         GCP Project Id
   --dataset         The Big Query dataset to verify or create
+CM360 Deployment Options:
+  --cm360-table            BQ table to read conversions from
+  --cm360-profile-id       CM360 profile id
+  --cm360-fl-activity-id   CM360 floodlight activity id
+  --cm360-fl-config-id     CM360 floodlight configuration id
 Deployment directives:
   --activate-apis   Activate all missing but required Cloud APIs
   --create-service-account
@@ -42,11 +47,44 @@ EOF
 
 function join { local IFS="$1"; shift; echo "$*"; }
 
+function cm360-json {
+cat <<EOF
+{
+  "table_name": "${CM360_TABLE}",
+  "topic": "cm360_conversion_upload",
+  "cm360_config": {
+    "profile_id": "${CM360_PROFILE_ID}",
+    "floodlight_activity_id": "${CM360_FL_ACTIVITY_ID}",
+    "floodlight_configuration_id": "${CM360_FL_CONFIG_ID}"
+  }
+}
+EOF
+}
+
+function get-roles {
+  gcloud projects get-iam-policy ${PROJECT} --flatten="bindings[].members" --format='table(bindings.role)' --filter="bindings.members:${SA_EMAIL}"
+}
+
+function deploy-timestamp {
+  # 2021-11-01-08-21-49
+  date +%Y-%m-%d-%H-%M-%S
+}
+
+function maybe-run {
+    if [ "${DRY_RUN:-}" = "echo" ]; then
+        echo "$@"
+    else
+        if [ "$VERBOSE" = "true" ]; then
+            echo "$@"
+        fi
+        "$@"
+    fi
+}
+
 # Switch definitions
 PROJECT=
 USER=
 DATASET="profitbidder"
-
 ACTIVATE_APIS=0
 BACKGROUND=0
 CREATE_SERVICE_ACCOUNT=0
@@ -54,11 +92,17 @@ USERNAME=0
 ADMIN=
 SERVICE_ACCOUNT_NAME="profit-bidder"
 STORAGE_BUCKET_NAME="conversion-upload_log"
+CF_REGION="us-central1"
+CM360_TABLE=""
+CM360_PROFILE_ID=""
+CM360_FL_ACTIVITY_ID=""
+CM360_FL_CONFIG_ID=""
 DEPLOY_BQ=0
 DEPLOY_CM360_FUNCTION=0
 DEPLOY_DELEGATOR=0
 DEPLOY_STORAGE=0
 DRY_RUN=""
+SA_ROLES="roles/bigquery.dataViewer roles/pubsub.publisher roles/iam.serviceAccountTokenCreator"
 VERBOSE=false
 
 # Command line parser
@@ -69,6 +113,18 @@ while [[ ${1:-} == -* ]] ; do
       ;;
     --dataset*)
       IFS="=" read _cmd DATASET <<< "$1" && [ -z ${DATASET} ] && shift && DATASET=$1
+      ;;
+    --cm360-table*)
+      IFS="=" read _cmd CM360_TABLE <<< "$1" && [ -z ${CM360_TABLE} ] && shift && CM360_TABLE=$1
+      ;;
+    --cm360-profile-id*)
+      IFS="=" read _cmd CM360_PROFILE_ID <<< "$1" && [ -z ${CM360_PROFILE_ID} ] && shift && CM360_PROFILE_ID=$1
+      ;;
+    --cm360-fl-activity-id*)
+      IFS="=" read _cmd CM360_FL_ACTIVITY_ID <<< "$1" && [ -z ${CM360_FL_ACTIVITY_ID} ] && shift && CM360_FL_ACTIVITY_ID=$1
+      ;;
+    --cm360-fl-config-id*)
+      IFS="=" read _cmd CM360_FL_CONFIG_ID <<< "$1" && [ -z ${CM360_FL_CONFIG_ID} ] && shift && CM360_FL_CONFIG_ID=$1
       ;;
     --deploy-all)
       DEPLOY_BQ=1
@@ -113,17 +169,6 @@ while [[ ${1:-} == -* ]] ; do
   shift
 done
 
-function maybe-run {
-    if [ "${DRY_RUN:-}" = "echo" ]; then
-        echo "$@"
-    else
-        if [ "$VERBOSE" = "true" ]; then
-            echo "$@"
-        fi
-        "$@"
-    fi
-}
-
 if [ "${DRY_RUN:-}" = "echo" ]; then
     echo "--dry-run enabled: commands will be echoed instead of executed"
 fi
@@ -134,7 +179,7 @@ if [ -z "${PROJECT}" ]; then
   exit
 fi
 
-USER=profit-bidder@${PROJECT}.iam.gserviceaccount.com
+SA_EMAIL=${SERVICE_ACCOUNT_NAME}@${PROJECT}.iam.gserviceaccount.com
 if [ ! -z ${ADMIN} ]; then
   _ADMIN="ADMINISTRATOR_EMAIL=${ADMIN}"
 fi
@@ -149,8 +194,10 @@ if [ ${ACTIVATE_APIS} -eq 1 ]; then
     "cloudbuild"
     "cloudfunctions"
     "cloudscheduler"
+    "dfareporting"
     "doubleclickbidmanager"
     "doubleclicksearch"
+    "iamcredentials"
     "pubsub"
     "storage-api"
   )
@@ -167,12 +214,20 @@ if [ ${ACTIVATE_APIS} -eq 1 ]; then
 fi
 
 # create service account
-SA_EMAIL=${SERVICE_ACCOUNT_NAME}@${PROJECT}.iam.gserviceaccount.com
 if [ ${CREATE_SERVICE_ACCOUNT} -eq 1 ]; then
   if !gcloud iam service-accounts describe $SA_EMAIL &> /dev/null; then 
     echo "Creating service account '${SERVICE_ACCOUNT_NAME}'"
     maybe-run gcloud iam service-accounts create ${SERVICE_ACCOUNT_NAME} --description 'Profit Bidder Service Account' --project ${PROJECT}
   fi
+  for role in ${SA_ROLES}; do
+    echo -n "Adding ${SERVICE_ACCOUNT_NAME} to ${role} "
+    if get-roles | grep $role &> /dev/null; then
+      echo "already added."
+    else
+      maybe-run gcloud projects add-iam-policy-binding ${PROJECT} --member="serviceAccount:${SA_EMAIL}" --role="${role}"
+      echo "added."
+    fi
+  done
 fi
 
 
@@ -193,12 +248,13 @@ fi
 if [ ${DEPLOY_BQ} -eq 1 ]; then
   echo "Creating BQ datasets"
   # Create dataset
-  echo "Creating datasets"  
   for dataset in sa360_data gmc_data business_data; do
-    echo "Creating BQ dataset: '${dataset}'" 
-    RETVAL=$?
+    echo -n "Creating BQ dataset: '${dataset}'" 
     if ! bq --project_id=${PROJECT} show --dataset ${dataset} > /dev/null 2>&1; then
       maybe-run bq --project_id=${PROJECT} mk --dataset ${dataset}
+      echo " created."
+    else
+      echo " already exists."
     fi
   done
 fi
@@ -206,31 +262,18 @@ fi
 echo "Deploy Delegator: ${DEPLOY_DELEGATOR}"
 # create cloud funtions
 if [ ${DEPLOY_DELEGATOR} -eq 1 ]; then
-  echo "Creating Cloud functions"
- # Create scheduled job
-  maybe-run gcloud beta scheduler jobs delete \
-    --location=us-central \
-    --project=${PROJECT} \
-    --quiet \
-    "delegator-scheduler" || echo "No job to delete" 
-
-  maybe-run gcloud beta scheduler jobs create pubsub \
-    "delegator" \
-    --location=us-central \
-    --schedule="0 6 * * *" \
-    --topic="conversion_upload_delegator" \
-    --message-body="RUN" \
-    --project=${PROJECT} || echo "scheduler failed!"
-
   echo "Deploying Delegator Cloud Function"
   pushd converion_upload_delegator
   maybe-run gcloud functions deploy "cloud_conversion_upload_delegator" \
-    --region=us-central1 \
+    --region=${CF_REGION} \
     --project=${PROJECT} \
     --trigger-topic=conversion_upload_delegator \
     --memory=2GB \
     --timeout=540s \
     --runtime python37 \
+    --update-env-vars="SA_EMAIL=${SA_EMAIL}" \
+    --service-account=${SA_EMAIL} \
+    --update-labels="deploy-timestamp=$(deploy-timestamp)" \
     --entry-point=main 
   popd
 fi
@@ -239,34 +282,44 @@ if [ ${DEPLOY_CM360_FUNCTION} -eq 1 ]; then
   echo "Creating CM360 Cloud Function"
  # Create scheduled job
   maybe-run gcloud beta scheduler jobs delete \
-    --location=us-central \
+    --location=${CF_REGION} \
     --project=${PROJECT} \
     --quiet \
     "cm360-scheduler" || echo "No job to delete"
 
   maybe-run gcloud beta scheduler jobs create pubsub \
     "cm360-scheduler" \
-    --location=us-central \
+    --location=${CF_REGION} \
+    --project=${PROJECT} \
     --schedule="0 6 * * *" \
     --topic="conversion_upload_delegator" \
-    --message-body="RUN" \
-    --project=${PROJECT} || echo "scheduler failed!"
+    --message-body='$(cm360-json)' || echo "scheduler failed!"
+
+  if [ "$VERBOSE" = "true" ]; then
+    echo
+    echo
+    echo "Delegator payload JSON:"
+    cm360-json
+    echo
+    echo
+  fi
 
   echo "Deploying CM360 Cloud Function"
-  pushd SA360_cloud_converion_upload_node
+  pushd CM360_cloud_conversion_upload_node
+  # TAKE NOTE: this will create the trigger topic in pubsub if it does not 
+  # already exist.
   maybe-run gcloud functions deploy "cm360_cloud_conversion_upload_node" \
-    --region=us-central1 \
+    --region=${CF_REGION} \
     --project=${PROJECT} \
     --trigger-topic=cm360_conversion_upload \
     --memory=256MB \
     --timeout=540s \
     --runtime python37 \
+    --update-env-vars="SA_EMAIL=${SA_EMAIL}" \
+    --service-account=${SA_EMAIL} \
+    --update-labels="deploy-timestamp=$(deploy-timestamp)" \
     --entry-point=main
   popd
 fi
 
 echo 'Script ran successfully!'
-# NOTES:
-# More automation may be needed?
-# * creation of pubsub topiocs
-# If gcloud scheduler fails, the user should be instructed to setup the scheduler in the cloud console
